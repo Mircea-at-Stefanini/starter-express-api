c6e5b55d63d3c87aecd20a2bf5496cea94f7a401
Mircea-at-Stefanini <129756559+Mircea-at-Stefanini@users.noreply.github.com>
2023-05-04 16:23:47 +0300

    provided a new number for test: 20205

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.json({"number": "12345"});
+    res.json({"number": "20305"});
 })
 app.listen(process.env.PORT || 3000)
